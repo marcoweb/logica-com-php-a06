06d9802a98bfe4b11d21995b9465e6cfc7325b0e
marcoweb <marcocarvalho.web@gmail.com>
2021-03-25 17:42:21 +0000

    Alt

--- index.php
+++ index.php
@@ -3,12 +3,21 @@
     <head>
         <meta charset="utf-8" />
         <title>Exemplo Template</title>
+        <style>
+            body {
+                font-family: sans-serif;
+            }
+        </style>
+        <link rel="stylesheet" href="estilo.css" />
     </head>
     <body>
     <?php include 'header.php' ?>
     <main>
         <h1>Página Principal</h1>
+        <button type="button" onclick="alert('Olá')">Exemplo</button>
     </main>
     <?php include 'footer.php' ?>
+
+    <script src="script.js"></script>
     </body>
 </html>
